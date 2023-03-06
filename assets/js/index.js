3c97b33f832569e20b7d97f99ddc54ca0be9d7ba
Kitenge Mukaba Jean-louis <jeanlouismukaba01@yahoo.com>
2023-03-06 23:58:09 +0100

    Add menu function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,13 @@
-function menu(){
-  const humberger = document.getElementById("humberger");
-  humberger.classList.add("active");
+function menu() {
+  const humberger = document.getElementById('humberger');
+  humberger.classList.add('active');
 }
-function closemenu(){
-  const humberger = document.getElementById("humberger");
-  humberger.classList.remove("active");
+function closemenu() {
+  const humberger = document.getElementById('humberger');
+  humberger.classList.remove('active');
 }
 
-// const humberger = document.getElementById("humberger");
-// const menu = document.getElementById("navbar");
-// const closemenu = document.getElementById("humbergerClose");
-
-// menu.addEventListener("click", () => {
-//   humberger.classList.add("active");
-// })
-// closemenu.addEventListener("click", () => {
-//   humberger.classList.remove("active");
-// })
+document.getElementById('navbar').onclick = () => {
+  menu();
+  closemenu();
+};
