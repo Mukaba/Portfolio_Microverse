b52485b8f722322b98646215774f81053c463c78
Kitenge Mukaba Jean-louis <jeanlouismukaba01@yahoo.com>
2023-03-06 21:50:55 +0100

    Add events on menu

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+function menu(){
+  const humberger = document.getElementById("humberger");
+  humberger.classList.add("active");
+}
+function closemenu(){
+  const humberger = document.getElementById("humberger");
+  humberger.classList.remove("active");
+}
