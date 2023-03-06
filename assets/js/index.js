5fb448415f49bed9ef60bd4fcd63a9ba1c220330
Kitenge Mukaba Jean-louis <jeanlouismukaba01@yahoo.com>
2023-03-06 22:58:06 +0100

    Fix linters errors

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,14 @@ function closemenu(){
   const humberger = document.getElementById("humberger");
   humberger.classList.remove("active");
 }
+
+// const humberger = document.getElementById("humberger");
+// const menu = document.getElementById("navbar");
+// const closemenu = document.getElementById("humbergerClose");
+
+// menu.addEventListener("click", () => {
+//   humberger.classList.add("active");
+// })
+// closemenu.addEventListener("click", () => {
+//   humberger.classList.remove("active");
+// })
